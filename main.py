e0f6fef1aafb2c33c11b489a8d3ae896b7618523
xkarina2197 <xkarina2197@gmail.com>
2025-11-24 16:05:40 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+# 1-misol
+text = input("text kirit: ")
+find = input("qidirilayotganni kirit: ")
+
+if find in text:
+    print("topildi")
+else:
+    print("topilmadi")
+
+# 2-misol
+text = input("text kirit: ")
+find = input("qidirilayotganni kirit:")
+
+if find in  text:
+    print("topildi")
+else:
+    print("topilmadi")
+
+
+# 3-misol
+text = input("text kirit: ")
+find = input("qidirilayotganni kirit: ")
+
+if find in text:
+    print("topildi")
+else:
+    print("topilmadi")
+
+# 4-misol
+text = (input("text kirit: "))
+find = (input("qidirilayotganni kirit: "))
+if find in text:
+    print("o'zb raqam")
+else:
+    print("boshgqa raqam")
+
+# 5-misol
+text = (input("text kirit: "))
+find = (input("qidirilayotganni kirit: "))
+if find in text:
+    print("ha email")
+else:
+    print("email emas")
